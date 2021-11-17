a3a48765dbf92503a375a226a67b96c0d6eb4c61
Izabele Almeida <izabele.almeida.1@hotmail.com>
2021-11-16 23:04:45 -0300

    Comandos customizados

--- cypress/integration/pre-cadastro.spec.js
+++ cypress/integration/pre-cadastro.spec.js
@@ -5,7 +5,7 @@ var faker = require('faker'); //biblioteca que serve para gerar dados dinamicos
 describe('Funcionalidade Pré Cadastro', () => {
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta')
     });
     
 it('Deve completar o pré cadastro com sucesso', () => {
@@ -25,11 +25,13 @@ it('Deve completar o pré cadastro com sucesso', () => {
     cy.get('#account_last_name').type(sobrenomeFaker) //metodo faker que gera segundo nome dinamicos.
 
     cy.get('.woocommerce-Button').click()
-
     cy.get('.woocommerce-message').should('contain' , 'Detalhes da conta modificados com sucesso.')
 
+});
 
-
+it.only('Deve completar pre cadastro usando comandos customizados', () => {
+        let emailFaker2 = faker.internet.email()
+        cy.preCadastro(emailFaker2, 'senha!@#forte', 'Izabele', 'Almeida')
 
 
 });
